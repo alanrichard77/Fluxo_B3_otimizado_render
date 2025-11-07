7ad2c5b2446d80092e159d5b159a8efaf02865cd
alanrichard77 <alanrichard_@outlook.com>
2025-11-07 19:34:46 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
-import json
-from dateutil import parser as dtparser
+from datetime import datetime
 
 def to_plotly_json(fig):
-    return json.loads(fig.to_json())
+    # garante serialização limpa
+    return {"data": fig.to_plotly_json().get("data", []),
+            "layout": fig.to_plotly_json().get("layout", {})}
 
-def parse_date(s):
-    if not s:
-        return None
-    try:
-        return dtparser.parse(s).date()
-    except Exception:
-        return None
+def parse_date(s: str) -> datetime:
+    for fmt in ("%Y-%m-%d", "%d/%m/%Y", "%Y/%m/%d"):
+        try:
+            return datetime.strptime(s, fmt)
+        except Exception:
+            continue
+    # fallback
+    return datetime.fromisoformat(s)
